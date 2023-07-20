9b294a728b254cd6c51c3b484b9e1bea68f3e0f6
mayuri <mayurivp@amazon.com>
2023-07-20 14:33:26 +0530

    ash changes

--- examples/gitlab_ci/cdk/infra-cdk-app/bin/custom-infra.ts
+++ examples/gitlab_ci/cdk/infra-cdk-app/bin/custom-infra.ts
@@ -13,6 +13,6 @@ new CustomInfraStack(app, "CustomInfraStack", {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '************', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
